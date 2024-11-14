5acfca1a29388d0beda6a32f474887da342237d2
Hengkubpom <hengkubpom@gmail.com>
2024-11-15 00:17:52 +0700

    Add: Array in function

--- index.js
+++ index.js
@@ -4,23 +4,29 @@
 function MergeArray(array1, array2){
     return array1.concat(array2);
 }
-
 console.log("MergeArray [1,2,3],[4,5,6]: ", MergeArray([1,2,3],[4,5,6]));
+
+
+
 
 //2
 function _filter(array){
     return array.filter((value) => value >= 30);
 }
-
 console.log("Filter [10,20,30,40,50]: ", _filter([10,20,30,40,50]));
 
+
+
+
 //3
-const myarray = [5,6,7,2,4];
-function RemoveIndex(index){
-    myarray.splice(index,1);
-    return myarray;
+function RemoveIndex(array,index){
+    array.splice(index,1);
+    return array;
 }
-console.log("RemoveIndex (2): ", RemoveIndex(2));
+console.log("RemoveIndex (2): ", RemoveIndex([5,6,7,2,4],2));
+
+
+
 
 
 //4
@@ -47,8 +53,13 @@ function RomanToNumber(roman){
     }
     return OverSum;
 }
-
 console.log("RomanNumber (XIV): ", RomanToNumber("XIV"));
+
+
+
+
+
+
 
 //5
 function RemoveAndSort(array){
@@ -61,24 +72,27 @@ function RemoveAndSort(array){
     NewArray.sort();
     return NewArray;
 }
-
 console.log("RemoveAndSort[1,2,1,4,5,2,3,4,1,4]:", RemoveAndSort([1,2,1,4,5,2,3,4,1,4]));
 
 
+
+
+
+
+
 //6
-let ForMove_Array = [1,2,3,4,5];
-function MoveArray(number){
+function MoveArray(array,number){
     number = number-1;
     let Moved_Array = [];
-    for(let i=0;i<ForMove_Array.length;i++){
-        if(i+number >= ForMove_Array.length){
-            Moved_Array[i] = ForMove_Array[(i+number)-ForMove_Array.length];
+    for(let i=0;i<array.length;i++){
+        if(i+number >= array.length){
+            Moved_Array[i] = array[(i+number)-array.length];
         }
         else{
-            Moved_Array[i] = ForMove_Array[i+number];
+            Moved_Array[i] = array[i+number];
         }
     }
     return Moved_Array;
 }
 
-console.log("MoveArray (2): ",MoveArray(2));
+console.log("MoveArray (3): ",MoveArray([1,2,3,4,5],3));
